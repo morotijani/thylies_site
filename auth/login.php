442abce332d3251401ac37acaae0625bcccd2831
Tijani Moro <tjhackx111@gmail.com>
2023-10-12 13:50:46 +0000

    initial commit

--- auth/login.php
+++ auth/login.php
@@ -2,9 +2,8 @@
 
 // USER SIGNIN
 
-require_once ('../db_connection/conn.php');
+require_once ('../connection/conn.php');
 $nav = 0;
-include ('inc/header-topnav.inc.php');
 
 if (user_is_logged_in()) {
     redirect(PROOT . 'shop/index');
@@ -63,7 +62,7 @@ if (isset($_POST['submit_login'])) {
 
 ?>
 
-    <section class="py-0">
+    <!-- <section class="py-0">
         <div class="container-fluid">
             <div class="row">
                 <div class="col-lg-6 px-0">
@@ -96,6 +95,98 @@ if (isset($_POST['submit_login'])) {
                 </div>
             </div>
         </div>
-    </section>
+    </section> -->
 
-<?php $follow = 0; include ('inc/footer.inc.php'); ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Sign in Page - Coach">
+    <meta name="keywords" content="">
+    <meta name="author" content="Codescandy">
+    <title>Sign in - Thylies</title>
+    <!-- Favicon icon-->
+    <link rel="shortcut icon" type="image/x-icon" href="<?= PROOT; ?>assets/media/logo/favicon.ico">
+
+    <link rel="stylesheet" href="<?= PROOT; ?>assets/css/bootstrap-icons.css">
+    <link rel="stylesheet" href="<?= PROOT; ?>assets/css/theme.min.css">
+
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-M8S4MT3EYG"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-M8S4MT3EYG');
+    </script>
+</head>
+<body class="bg-light">
+
+    <div class="d-flex align-items-center position-relative vh-100">
+        <div class="container">
+            <div class="row g-0">
+                <div class="col-md-8 col-lg-7 col-xl-6 offset-md-2 offset-lg-2 offset-xl-3 space-top-3 space-lg-0">
+                    <a href="<?= PROOT; ?>" class="mb-4 d-flex justify-content-center">
+                        <img src="<?= PROOT; ?>assets/media/logo/logo.jpg" alt="logo">
+                    </a>
+
+                    <div class="bg-white p-4 p-xl-6 p-xxl-8 p-lg-4 rounded-3 border">
+                        <form>
+                            <h1 class="mb-1 text-center h3">Welcome</h1>
+                            <p class="mb-4 text-center">Sign in using your thylies credentials.</p>
+                            <div class="mb-3">
+                                <label for="email" class="form-label">Email<span class="text-danger">*</span> </label>
+                                <input type="email" id="email" class="form-control " placeholder="Email address"
+                                    required="">
+                            </div>
+                            <div class="mb-3 mb-4">
+                                <label for="password" class="form-label">Password<span
+                                        class="text-danger">*</span></label>
+                                <input type="password" id="password" class="form-control" placeholder="Password"
+                                    required="">
+                            </div>
+                            <div class="d-grid">
+                                <button class="btn btn-warning" type="submit">
+                                    Sign in
+                                </button>
+                            </div>
+                            <div class="d-xxl-flex justify-content-between mt-4 ">
+                                <p class="text-muted font-14 mb-0">
+                                    Don't have an account yet? <a href="signup-cover.html">Sign up</a>
+                                </p>
+                                <p class="font-14 mb-0">
+                                    <a href="password-reset-cover.html">Forget Password</a>
+                                </p>
+                            </div>
+                        </form>
+                    </div>
+                    <div class="mt-3 nav-footer-links">
+                        <ul class="nav">
+                            <li class="nav-item">
+                                <a class="nav-link active" href="#">Privacy Policy </a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Terms & Conditions</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+    <script src="<?= PROOT; ?>assets/js/jquery.min.js"></script>
+    <script src="<?= PROOT; ?>assets/js/bootstrap.bundle.min.js"></script>
+    <script src="<?= PROOT; ?>assets/js/jquery.slimscroll.min.js"></script>
+    <script src="<?= PROOT; ?>assets/js/theme.min.js"></script>
+
+</body>
+
+</html>
--- index.php
+++ index.php
@@ -23,7 +23,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Mansalva&family=Patrick+Hand&family=Young+Serif&display=swap" rel="stylesheet">
 
     <!-- Favicon icon-->
-    <link rel="shortcut icon" type="image/x-icon" href="<?= PROOT; ?>assets/media/logo/favicon.ico">
+    <link rel="shortcut icon" type="image/x-icon" href="<?= PROOT; ?>assets/media/logo/logo-min.png">
 
     <link rel="stylesheet" href="<?= PROOT; ?>assets/css/bootstrap-icons.css">
     <link rel="stylesheet" href="<?= PROOT; ?>assets/css/theme.min.css">
@@ -44,7 +44,7 @@
     <!-- navigation start -->
     <nav class="navbar navbar-expand-lg navbar-dark navbar-default fixed-top border-3 border-top border-warning">
         <div class="container">
-            <a class="navbar-brand" href="<?= PROOT; ?>"><img src="<?= PROOT; ?>assets/media/coach-yoga.svg"
+            <a class="navbar-brand" href="<?= PROOT; ?>"><img src="<?= PROOT; ?>assets/media/logo/logo.jpg"
                  alt=""></a>
             <button class="navbar-toggler collapsed" type="button" data-bs-toggle="collapse"
              data-bs-target="#navbar-default" aria-controls="navbar-default" aria-expanded="false"
@@ -373,89 +373,89 @@
 
   </main>
 
-  <!-- footer -->
-<div class="footer pt-11 pb-3 bg-dark text-body">
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-2 col-md-4 col-6">
-                <div class="mb-4">
-                    <h4 class="mb-4 text-white">About</h4>
-                    <ul class="list-unstyled list-group">
-                        <li class="list-group-item"><a href="#" class="list-group-item-link">Thylies</a></li>
-                        <li class="list-group-item"><a href="#" class="list-group-item-link">Scholarship list</a></li>
-                        <li class="list-group-item"><a href="#" class="list-group-item-link">Press</a></li>
-                        <li class="list-group-item"><a href="#" class="list-group-item-link">Social Feed</a></li>
-                        <li class="list-group-item"><a href="#" class="list-group-item-link"> Help</a></li>
-                        <li class="list-group-item"><a href="#" class="list-group-item-link">Contact Us</a></li>
-                    </ul>
-                </div>
-            </div>
-            <div class="col-lg-2 col-md-4 col-6">
-                <div class="mb-4">
-                    <h4 class="mb-4 text-white ">Resources</h4>
-                    <ul class="list-unstyled list-group ">
-                        <li class="list-group-item"><a href="<?= PROOT; ?>donate" class="list-group-item-link">Donate</a></li>
-                        <li class="list-group-item"><a href="<?= PROOT; ?>scholarships" class="list-group-item-link">Scholarship</a></li>
-                        <li class="list-group-item"><a href="<?= PROOT; ?>sanitary-welfare" class="list-group-item-link">Sanitary Welfare</a></li>
-                        <li class="list-group-item"><a href="<?= PROOT; ?>student-in-business" class="list-group-item-link">Student in Business</a></li>
-                        <li class="list-group-item"><a href="<?= PROOT; ?>auth/register" class="list-group-item-link">Join us</a></li>
-                        <li class="list-group-item"><a href="<?= PROOT; ?>auth/login" class="list-group-item-link">Login</a></li>
-                    </ul>
-                </div>
-            </div>
-            <div class="col-lg-2 col-md-4 col-6">
-                <div class="mb-4">
-                    <h4 class="mb-4 text-white ">Follow Me</h4>
-                    <ul class="list-unstyled list-group">
-                        <li class="list-group-item"><a href="https://" class="list-group-item-link">Instagram</a></li>
-                        <li class="list-group-item"><a href="https://" class="list-group-item-link"> Facebook</a></li>
-                        <li class="list-group-item"><a href="https://" class="list-group-item-link"> LinkedIn</a></li>
-                        <li class="list-group-item"><a href="https://" class="list-group-item-link">YouTube</a></li>
-                        <li class="list-group-item"><a href="https://" class="list-group-item-link">Twitter</a></li>
-                    </ul>
-                </div>
-            </div>
-            <div class="col-lg-4 col-md-8 col-12">
-                <div class="mb-4">
-                    <h4 class="mb-4 text-white">Subscribe to our newsletter</h4>
-                    <div>
-                        <p>Subscribe to get notified daily new motivational & inspiration tips.</p>
-                        <form>
-                            <div class="mb-3">
-                                <input type="email" class="form-control border-white" placeholder="Email Address" required>
-                            </div>
-                            <button type="submit" class="btn btn-warning">Subscribe</button>
-                        </form>
+    <!-- footer -->
+    <div class="footer pt-11 pb-3 bg-dark text-body">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-2 col-md-4 col-6">
+                    <div class="mb-4">
+                        <h4 class="mb-4 text-white">About</h4>
+                        <ul class="list-unstyled list-group">
+                            <li class="list-group-item"><a href="#" class="list-group-item-link">Thylies</a></li>
+                            <li class="list-group-item"><a href="#" class="list-group-item-link">Scholarship list</a></li>
+                            <li class="list-group-item"><a href="#" class="list-group-item-link">Press</a></li>
+                            <li class="list-group-item"><a href="#" class="list-group-item-link">Social Feed</a></li>
+                            <li class="list-group-item"><a href="#" class="list-group-item-link"> Help</a></li>
+                            <li class="list-group-item"><a href="#" class="list-group-item-link">Contact Us</a></li>
+                        </ul>
                     </div>
                 </div>
-            </div>
-            <div class="col-md-12">
-                <div class="mt-8">
-                    <ul class="list-inline">
-                        <li class="list-inline-item">Copyright &copy; 2020<span id="copyright"> -
-                            <script>document.getElementById('copyright').appendChild(document.createTextNode(new Date().getFullYear()))</script>
-                        </span>, Inc.</li>
-                        <li class="list-inline-item"><a href="<?= PROOT; ?>privacy-policy" class="text-reset"> Privacy Policy </a></li>
-                        <li class="list-inline-item"><a href="<?= PROOT; ?>terms" class="text-reset"> Terms</a></li>
-                    </ul>
+                <div class="col-lg-2 col-md-4 col-6">
+                    <div class="mb-4">
+                        <h4 class="mb-4 text-white ">Resources</h4>
+                        <ul class="list-unstyled list-group ">
+                            <li class="list-group-item"><a href="<?= PROOT; ?>donate" class="list-group-item-link">Donate</a></li>
+                            <li class="list-group-item"><a href="<?= PROOT; ?>scholarships" class="list-group-item-link">Scholarship</a></li>
+                            <li class="list-group-item"><a href="<?= PROOT; ?>sanitary-welfare" class="list-group-item-link">Sanitary Welfare</a></li>
+                            <li class="list-group-item"><a href="<?= PROOT; ?>student-in-business" class="list-group-item-link">Student in Business</a></li>
+                            <li class="list-group-item"><a href="<?= PROOT; ?>auth/register" class="list-group-item-link">Join us</a></li>
+                            <li class="list-group-item"><a href="<?= PROOT; ?>auth/login" class="list-group-item-link">Login</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-2 col-md-4 col-6">
+                    <div class="mb-4">
+                        <h4 class="mb-4 text-white ">Follow Me</h4>
+                        <ul class="list-unstyled list-group">
+                            <li class="list-group-item"><a href="https://" class="list-group-item-link">Instagram</a></li>
+                            <li class="list-group-item"><a href="https://" class="list-group-item-link"> Facebook</a></li>
+                            <li class="list-group-item"><a href="https://" class="list-group-item-link"> LinkedIn</a></li>
+                            <li class="list-group-item"><a href="https://" class="list-group-item-link">YouTube</a></li>
+                            <li class="list-group-item"><a href="https://" class="list-group-item-link">Twitter</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-8 col-12">
+                    <div class="mb-4">
+                        <h4 class="mb-4 text-white">Subscribe to our newsletter</h4>
+                        <div>
+                            <p>Subscribe to get notified daily new motivational & inspiration tips.</p>
+                            <form>
+                                <div class="mb-3">
+                                    <input type="email" class="form-control border-white" placeholder="Email Address" required>
+                                </div>
+                                <button type="submit" class="btn btn-warning">Subscribe</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-12">
+                    <div class="mt-8">
+                        <ul class="list-inline">
+                            <li class="list-inline-item">Copyright &copy; 2020<span id="copyright"> -
+                                <script>document.getElementById('copyright').appendChild(document.createTextNode(new Date().getFullYear()))</script>
+                            </span>, Inc.</li>
+                            <li class="list-inline-item"><a href="<?= PROOT; ?>privacy-policy" class="text-reset"> Privacy Policy </a></li>
+                            <li class="list-inline-item"><a href="<?= PROOT; ?>terms" class="text-reset"> Terms</a></li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
-  <!-- Optional JavaScript -->
+    <!-- Optional JavaScript -->
 
 
-  <!-- Libs JS -->
-<script src="<?= PROOT; ?>assets/js/jquery.min.js"></script>
-<script src="<?= PROOT; ?>assets/js/bootstrap.bundle.min.js"></script>
-<script src="<?= PROOT; ?>assets/js/jquery.slimscroll.min.js"></script>
+    <!-- Libs JS -->
+    <script src="<?= PROOT; ?>assets/js/jquery.min.js"></script>
+    <script src="<?= PROOT; ?>assets/js/bootstrap.bundle.min.js"></script>
+    <script src="<?= PROOT; ?>assets/js/jquery.slimscroll.min.js"></script>
 
 
 
-<!-- Theme JS -->
-<script src="<?= PROOT; ?>assets/js/theme.min.js"></script>
-<script src="<?= PROOT; ?>assets/plyr.min.js"></script>
+    <!-- Theme JS -->
+    <script src="<?= PROOT; ?>assets/js/theme.min.js"></script>
+    <script src="<?= PROOT; ?>assets/plyr.min.js"></script>
 
 </body>
 </html>
