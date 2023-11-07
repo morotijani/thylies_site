424c9b6ad38af01030f45a7d932b2f97407ac4fb
Tijani Moro <tjhackx111@gmail.com>
2023-11-07 00:28:57 +0000

    initial commit

--- admin/SW/rejected.php
+++ admin/SW/rejected.php
@@ -87,7 +87,7 @@
     // SEARCH AND PAGINATION FOR LIST
     function load_data(page, query = '') {
         $.ajax({
-            url : "<?= PROOT; ?>parsers/scholarship.rejected.list.php",
+            url : "<?= PROOT; ?>parsers/sw.rejected.list.php",
             method : "POST",
             data : {
                 page : page, 
