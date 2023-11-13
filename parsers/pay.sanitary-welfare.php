7500c79cede182447f3c261e035296822add87e4
Tijani Moro <tjhackx111@gmail.com>
2023-11-13 21:53:44 +0000

    initial commit

--- parsers/pay.sanitary-welfare.php
+++ parsers/pay.sanitary-welfare.php
@@ -7,23 +7,23 @@
 	if (isset($_POST['reference'])) {
 		$reference = sanitize($_POST['reference']);
 		$email = sanitize($_POST['email']);
-		$scholarship_id = sanitize($_POST['scholarship_id']);
+		$sw_id = sanitize($_POST['sw_id']);
 		$createdAt = date("Y-m-d H:m:s");
 
-		if ($conn->query("SELECT * FROM thylies_scholarship WHERE scholarship_id = '".$scholarship_id."' LIMIT 1")->rowCount() > 0) {
+		if ($conn->query("SELECT * FROM thylies_sanitary_welfare WHERE sw_id = '".$sw_id."' LIMIT 1")->rowCount() > 0) {
 			$sql = "
 				INSERT INTO thylies_transactions (transaction_id, from_id, transaction_service, status, createdAt) 
 				VALUES (?, ?, ?, ?, ?)
 			";
 			$statement = $conn->prepare($sql);
-			$result = $statement->execute([$reference, $scholarship_id, 'scholarship', 1, $createdAt]);
+			$result = $statement->execute([$reference, $sw_id, 'sanitarywelfare', 1, $createdAt]);
 
 			if (isset($result)) {
 				$_SESSION['pay_id'] = $reference;
 
-				foreach ($conn->query("SELECT * FROM thylies_scholarship WHERE scholarship_id = '".$scholarship_id."' LIMIT 1")->fetchAll() as $key) {
+				foreach ($conn->query("SELECT * FROM thylies_sanitary_welfare WHERE sw_id = '".$sw_id."' LIMIT 1")->fetchAll() as $key) {
 					// code...
-					if ($conn->query("SELECT * FROM thylies_user WHERE user_unique_id = '".$key['user_id']."' AND user_email = NULL OR user_email = '' LIMIT 1")->rowCount() > 0) {
+					if ($conn->query("SELECT * FROM thylies_user WHERE user_unique_id = '".$key['user_id']."' LIMIT 1")->rowCount() > 0) {
 						$updateQuery = "
 							UPDATE thylies_user 
 							SET user_email = ? 
