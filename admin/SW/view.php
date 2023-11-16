8d3dc0ad0fbe98f8f577514e2e10763c6cf47c51
Tijani Moro <tjhackx111@gmail.com>
2023-11-16 16:08:42 +0000

    initial commit

--- admin/SW/view.php
+++ admin/SW/view.php
@@ -149,7 +149,6 @@
                         <div class="ms-5">
                             <button for="file-upload" class="btn btn-sm btn-neutral" data-bs-toggle="modal" data-bs-target="#grantModal"><span>Grant</span></button>
                             <?php 
-                                
                                 if ($conn->query("SELECT * FROM thylies_transactions WHERE from_id = '".$sw_id."' AND transaction_service = 'sanitarywelfare' AND status = 1")->rowCount() > 0) {
                                     echo '
                                         <button class="btn btn-sm btn-success"><span><i class="bi bi-cash"></i> Paid</span></button>
