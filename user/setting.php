b25715dfeb10f56c865b404c4955c83f84d154e9
Tijani Moro <tjhackx111@gmail.com>
2023-11-23 16:30:58 +0000

    initial commit

--- user/setting.php
+++ user/setting.php
@@ -90,74 +90,75 @@
 						</div>
 						<div class="card-body p-4">
 							<div class="">
-								<!-- form -->
-								<form class="row" method="POST" id="settingsForm">
-									<div class="mb-3 col-12 col-md-12">
-										<label class="form-label" for="user_fullname">Name of Student<span class="text-danger">*</span></label>
-										<input type="text" id="user_fullname" name="user_fullname" class="form-control" required value="<?= $user_fullname; ?>">
-									</div>
-									<div class="mb-3 col-12 col-md-12">
-										<label class="form-label" for="user_email">Email<span class="text-danger">*</span></label>
-										<input type="email" id="user_email" name="user_email" class="form-control" required value="<?= $user_email; ?>">
-									</div>
-									<div class="mb-3 col-12 col-md-12">
-										<label class="form-label" for="user_phone">Phone<span class="text-danger">*</span></label>
-										<input type="number" min="1" id="user_phone" name="user_phone" class="form-control" required value="<?= $user_phone; ?>">
-									</div>
-                                    <div class="mb-3 col-12 col-md-12">
-                                        <label class="form-label" for="user_school_name">School Name<span class="text-danger">*</span></label>
-                                        <input type="number" min="1" id="user_school_name" name="user_school_name" class="form-control" required value="<?= $user_school_name; ?>">
-                                    </div>
-                                    <div class="mb-3 col-12 col-md-12">
-                                        <label class="form-label" for="user_index_number">Index Number<span class="text-danger">*</span></label>
-                                        <input type="number" min="1" id="user_index_number" name="index_number" class="form-control" required value="<?= $user_index_number; ?>">
-                                    </div>
-                                    <div class="mb-3 col-12 col-md-12">
-                                        <label class="form-label" for="user_gender">Gender<span class="text-danger">*</span></label>
-                                        <input type="number" min="1" id="user_gender" name="user_gender" class="form-control" required value="<?= $user_gender; ?>">
-                                    </div>
-                                    <div class="mb-3 col-12 col-md-12">
-                                        <label class="form-label" for="user_country">Country</label>
-                                        <input type="number" min="1" id="user_country" name="user_country" class="form-control" value="<?= $user_country; ?>">
-                                    </div>
-                                    <div class="mb-3 col-12 col-md-12">
-                                        <label class="form-label" for="user_state">State / Region</label>
-                                        <input type="number" min="1" id="user_state" name="user_state" class="form-control" value="<?= $user_state; ?>">
-                                    </div>
-                                    <div class="mb-3 col-12 col-md-12">
-                                        <label class="form-label" for="user_city">City</label>
-                                        <input type="number" min="1" id="user_city" name="user_city" class="form-control" value="<?= $user_city; ?>">
-                                    </div>
-                                    <div class="mb-3 col-12 col-md-12">
-                                        <label class="form-label" for="user_address">Address<span class="text-danger">*</span></label>
-                                        <input type="number" min="1" id="user_address" name="user_address" class="form-control" value="<?= $user_address; ?>">
-                                    </div>
-                                    <div class="mb-3 col-12 col-md-12">
-                                        <label class="form-label" for="user_postcode">Post Code</label>
-                                        <input type="number" min="1" id="user_postcode" name="user_postcode" class="form-control" value="<?= $user_postcode; ?>">
-                                    </div>
+                                <?= $user_verified; ?>
+                            </div>
+							<!-- form -->
+							<form class="row" method="POST" id="settingsForm">
+								<div class="mb-3 col-12 col-md-12">
+									<label class="form-label" for="user_fullname">Name of Student<span class="text-danger">*</span></label>
+									<input type="text" id="user_fullname" name="user_fullname" class="form-control" required value="<?= $user_fullname; ?>">
+								</div>
+								<div class="mb-3 col-12 col-md-12">
+									<label class="form-label" for="user_email">Email<span class="text-danger">*</span></label>
+									<input type="email" id="user_email" name="user_email" class="form-control" required value="<?= $user_email; ?>">
+								</div>
+								<div class="mb-3 col-12 col-md-12">
+									<label class="form-label" for="user_phone">Phone<span class="text-danger">*</span></label>
+									<input type="number" min="1" id="user_phone" name="user_phone" class="form-control" required value="<?= $user_phone; ?>">
+								</div>
+                                <div class="mb-3 col-12 col-md-12">
+                                    <label class="form-label" for="user_school_name">School Name<span class="text-danger">*</span></label>
+                                    <input type="number" min="1" id="user_school_name" name="user_school_name" class="form-control" required value="<?= $user_school_name; ?>">
+                                </div>
+                                <div class="mb-3 col-12 col-md-12">
+                                    <label class="form-label" for="user_index_number">Index Number<span class="text-danger">*</span></label>
+                                    <input type="number" min="1" id="user_index_number" name="index_number" class="form-control" required value="<?= $user_index_number; ?>">
+                                </div>
+                                <div class="mb-3 col-12 col-md-12">
+                                    <label class="form-label" for="user_gender">Gender<span class="text-danger">*</span></label>
+                                    <input type="number" min="1" id="user_gender" name="user_gender" class="form-control" required value="<?= $user_gender; ?>">
+                                </div>
+                                <div class="mb-3 col-12 col-md-12">
+                                    <label class="form-label" for="user_country">Country</label>
+                                    <input type="number" min="1" id="user_country" name="user_country" class="form-control" value="<?= $user_country; ?>">
+                                </div>
+                                <div class="mb-3 col-12 col-md-12">
+                                    <label class="form-label" for="user_state">State / Region</label>
+                                    <input type="number" min="1" id="user_state" name="user_state" class="form-control" value="<?= $user_state; ?>">
+                                </div>
+                                <div class="mb-3 col-12 col-md-12">
+                                    <label class="form-label" for="user_city">City</label>
+                                    <input type="number" min="1" id="user_city" name="user_city" class="form-control" value="<?= $user_city; ?>">
+                                </div>
+                                <div class="mb-3 col-12 col-md-12">
+                                    <label class="form-label" for="user_address">Address<span class="text-danger">*</span></label>
+                                    <input type="number" min="1" id="user_address" name="user_address" class="form-control" value="<?= $user_address; ?>">
+                                </div>
+                                <div class="mb-3 col-12 col-md-12">
+                                    <label class="form-label" for="user_postcode">Post Code</label>
+                                    <input type="number" min="1" id="user_postcode" name="user_postcode" class="form-control" value="<?= $user_postcode; ?>">
+                                </div>
 
-                                    <div class="col-12">
-                                        <button type="button" class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#settingsModalCenter">Submit</button>
-                                    </div>
+                                <div class="col-12">
+                                    <button type="button" class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#settingsModalCenter">Submit</button>
+                                </div>
 
-                                    <!-- Modal -->
-                                    <div class="modal fade" id="settingsModalCenter" tabindex="-1" role="dialog" aria-labelledby="settingsModalCenterTitle" aria-hidden="true">
-                                        <div class="modal-dialog modal-dialog-centered" role="document">
-                                            <div class="modal-content">
-                                                <div class="modal-body">
-                                                    Are you sure you ant to make these changes?
-                                                </div>
-                                                <div class="modal-footer">
-                                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                                                    <button class="g-recaptcha btn btn-warning" data-sitekey="<?= RECAPTCHA_SITE_KEY; ?>" data-callback='submit_settings_form' data-action='submit' type="submit">Yes</button>
-                                                </div>
+                                <!-- Modal -->
+                                <div class="modal fade" id="settingsModalCenter" tabindex="-1" role="dialog" aria-labelledby="settingsModalCenterTitle" aria-hidden="true">
+                                    <div class="modal-dialog modal-dialog-centered" role="document">
+                                        <div class="modal-content">
+                                            <div class="modal-body">
+                                                Are you sure you ant to make these changes?
+                                            </div>
+                                            <div class="modal-footer">
+                                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                                <button class="g-recaptcha btn btn-warning" data-sitekey="<?= RECAPTCHA_SITE_KEY; ?>" data-callback='submit_settings_form' data-action='submit' type="submit">Yes</button>
                                             </div>
                                         </div>
                                     </div>
+                                </div>
 
-								</form>
-							</div>
+							</form>
 						</div>
 					</div>
 				</div>
