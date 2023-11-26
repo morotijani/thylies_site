f3d16877bd3b87b8cd5e8b2273ad7c37b3be5ab0
Tijani Moro <tjhackx111@gmail.com>
2023-11-26 16:05:52 +0000

    initial commit

--- admin/admin.about.php
+++ admin/admin.about.php
@@ -44,25 +44,6 @@
 
 
 ?>
-
-    <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-        <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-            <h1 class="h2">About us.</h1>
-            <div class="btn-toolbar mb-2 mb-md-0">
-                <div class="btn-group me-2">
-                    <a href="<?= PROOT; ?>admin/about" class="btn btn-sm btn-outline-secondary">Refresh</a>
-                    <a href="<?= PROOT; ?>admin/contacts" class="btn btn-sm btn-outline-secondary">Contacts</a>
-                </div>
-                <a href="<?= PROOT; ?>admin/index" class="btn btn-sm btn-outline-secondary">
-                    <span data-feather="home"></span>
-                    Home
-                </a>
-            </div>
-        </div>
-
-    
-    </main>
-
     <!--  -->
     <header>
         <div class="container-fluid">
