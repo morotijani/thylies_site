9f8a0d375fdd4a43f57d127405f713a94f0b9f7c
Tijani Moro <tjhackx111@gmail.com>
2023-11-28 17:44:26 +0000

    initial commit

--- inc/header.inc.php
+++ inc/header.inc.php
@@ -65,7 +65,7 @@
                         <a href="<?= PROOT; ?>" class="nav-link">Home</a>
                     </li>
                     <li class="nav-item">
-                        <a href=" <?= PROOT; ?>scholarships" class="nav-link">Scholarships</a>
+                        <a href=" <?= PROOT; ?>scholarship" class="nav-link">Scholarship</a>
                     </li>
                     <li class="nav-item">
                         <a href=" <?= PROOT; ?>sanitary-welfare" class="nav-link">Sanitary Welfare</a>
