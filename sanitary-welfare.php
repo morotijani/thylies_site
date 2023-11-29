a1627269623345da59fbae825015064327a83253
Tijani Moro <tjhackx111@gmail.com>
2023-11-29 05:56:38 +0000

    initial commit

--- sanitary-welfare.php
+++ sanitary-welfare.php
@@ -20,12 +20,33 @@
                             <p class="px-xl-18 px-md-10 mb-5 lead" style="color: #333;">
                                 Thylies offers you a variety of  choices tailored to meet your preference of Sanitary items
                             </p>
-                            <a href="pages/signin.html" class="btn btn-primary btn-lg">Get Ticket</a>
+                            <a href="<?= PROOT; ?>user/apply-sanitary-welfare" class="btn btn-warning btn-lg">Application form</a>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-  <!--hero section-->
 
+         <!-- course -->
+        <div class="pt-lg-13 pb-lg-14 py-8 bg-dark">
+            <div class="container">
+                <div class="row">
+                    <div class="offset-lg-2 col-lg-8 col-md-12 col-12">
+                        <div class="text-center mb-12">
+                            <!--title-->
+                            <h2 class="text-white mb-3 h1">
+                                Lead Your Field. Impact the World.
+                            </h2>
+                            <!--para-->
+                            <p class="lead mb-5">
+                                We draw on our 25-year track record to coach established and emerging leaders to identify
+                                their purpose, thrive personally & professionally and so improve business performance.
+                            </p>
+                            <!--button-->
+                            <a href="#" class="btn btn-secondary btn-lg">Get Started</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
 <?php include ('inc/footer.inc.php'); ?>
