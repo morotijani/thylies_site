d1f923766b60f2ccb284e1a59fbabad10854afc5
Tijani Moro <tjhackx111@gmail.com>
2023-11-29 05:50:56 +0000

    initial commit

--- sanitary-welfare.php
+++ sanitary-welfare.php
@@ -9,24 +9,23 @@
 
 ?>
      <!-- hero section -->
-  <div
-    style="background: url(<?= PROOT; ?>assets/media/sanitary-welfare/bg-3.jpg); background-size: cover; background-repeat: no-repeat; background-position: center;">
-    <div class="container">
-      <div class="row">
-        <div class="offset-lg-2 col-lg-8 col-md-12 col-12">
-          <div class="pt-17 pb-13 pt-lg-19 pb-lg-19 text-center text-light">
-            <h1 class="display-3 text-white mb-3 lh-1">
-              Become the hero of your own story
-            </h1>
-            <p class="px-xl-18 px-md-10 mb-5 lead">
-              Join our Coach community of like-minded individuals. Get your ticket to the 2021 event.
-            </p>
-            <a href="pages/signin.html" class="btn btn-primary btn-lg">Get Ticket</a>
-          </div>
+        <div style="background: url(<?= PROOT; ?>assets/media/sanitary-welfare/bg-3.jpg); background-size: cover; background-repeat: no-repeat; background-position: center;">
+            <div class="container">
+                <div class="row">
+                    <div class="offset-lg-2 col-lg-8 col-md-12 col-12">
+                        <div class="pt-17 pb-13 pt-lg-19 pb-lg-19 text-center text-light">
+                            <h1 class="display-3 text-white mb-3 lh-1">
+                                Sanitary Welfare
+                            </h1>
+                            <p class="px-xl-18 px-md-10 mb-5 lead">
+                                Thylies offers you a variety of  choices tailored to meet your preference of Sanitary items
+                            </p>
+                            <a href="pages/signin.html" class="btn btn-primary btn-lg">Get Ticket</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  </div>
   <!--hero section-->
 
 <?php include ('inc/footer.inc.php'); ?>
