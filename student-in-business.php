98ce30c2bdb989cdb9f667ed5fee95aac72fa89d
Tijani Moro <tjhackx111@gmail.com>
2023-11-30 17:37:13 +0000

    initial commit

--- student-in-business.php
+++ student-in-business.php
@@ -50,6 +50,33 @@
             </div>
         </div>
 
+
+        <!-- prgoram -->
+        <div class="py-lg-12 py-8">
+            <div class="container">
+                <div class="mb-9">
+                    <div class="row align-items-center">
+                        <div class="col-xl-6 col-lg-6 col-md-6 col-12">
+                            <!--heading-->
+                            <h2 class="h1">How to get Started with the FUND</h2>
+                        </div>
+                        <div class=" col-xl-6 col-lg-6 col-md-6 col-12">
+                            <!--para-->
+                            <p class="lead">
+                                Outline your business, describe what you do or the sort of business you intend to take on.
+                                <br>
+                                <br>
+                                State your budget for starting your intended business or expanding your already existing business
+                                <br>
+                                <br>
+                                A date would be set for approval and disbursement of funds and assigning resource personnels
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
         <!-- features -->
         <div class="py-lg-18 py-12 bg-light">
             <div class="container">
@@ -146,11 +173,32 @@
                             </div>
                         </div>
                     </div>
+                    <div class="col-md-6 col-12">
+                        <div class="card rounded-3 mb-5">
+                            <div class="p-5 d-flex justify-content-between
+                                align-items-center">
+                                <div>
+                                    <h4 class="mb-1">5</h4>
+                                    <p class="px-lg-7 lead">How much capital do you need to grow your business?</p>
+                                </div>
+                                <div>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="34" height="34" viewBox="0 0 24
+                                        24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                                      stroke-linejoin="round" class="feather
+                                        feather-award text-primary">
+                                      <circle cx="12" cy="8" r="7"></circle>
+                                      <polyline points="8.21 13.89 7 23 12 20 17 23
+                                          15.79 13.88"></polyline>
+                                    </svg>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
                 <div class="text-center">
-                    <p class="lead mb-0">We've got you covered all semester all year.</p>
+                    <p class="lead mb-0">We would make necessary provisions for your startup business to grow and develop.</p>
                     <br>
-                    <a href="<?= PROOT; ?>user/apply-sanitary-welfare" class="btn btn-outline-warning">Apply Now!</a>
+                    <a href="<?= PROOT; ?>user/apply-student-in-business" class="btn btn-outline-warning">Apply Now!</a>
                 </div>
             </div>
         </div>
